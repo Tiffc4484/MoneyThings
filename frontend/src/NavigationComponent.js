e15d60ad9bc6336915e330f70753a422b44ad5e7
Tiffc4484 <tiffanyc4484@gmail.com>
2021-04-24 00:51:27 -0700

    fix nav bar and functional nav bar color contrast

--- frontend/src/NavigationComponent.js
+++ frontend/src/NavigationComponent.js
@@ -25,14 +25,16 @@ export default function NavigationComponent(props) {
     <nav className="navbar navbar-expand-lg navbar-dark px-3">
       <div className="container-fluid d-flex">
         <Link className="navbar-brand" to="/">
-          <img
-            src={"../images/MoneyEmoji.png"}
-            alt="logo"
-            width="30"
-            height="24"
-            className="d-inline-block align-text-top me-2"
-          />
-          <h1>MoneyThings</h1>
+          <div className="d-flex">
+            <img
+              src={"../images/MoneyEmoji.png"}
+              alt="logo"
+              width="60"
+              height="48"
+              className="d-inline-block align-text-top me-2"
+            />
+            <h1>MoneyThings</h1>
+          </div>
         </Link>
         {!props.user ? (
           <div className="nav-link" style={{ color: "rgba(255,255,255,1)" }}>
@@ -51,7 +53,7 @@ export default function NavigationComponent(props) {
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
-                  style={{ color: "rgba(255,255,255,0.8)" }}
+                  style={{ color: "rgba(255,255,255, 0.8)" }}
                 >
                   Welcome, {props.user.username}
                 </div>
